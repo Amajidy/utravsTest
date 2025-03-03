436016d07e03e49f8b482a113c4838f9eaf1f054
Amir <majidy.amirhossein@gmail.com>
2025-03-03 23:35:59 +0330

    fix all image routes

--- src/app/api/infra/movies.infra.ts
+++ src/app/api/infra/movies.infra.ts
@@ -2,6 +2,7 @@ import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {MovieId, Movies} from '../entities/movies.entity';
+import {map} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,15 @@ export class MoviesInfra{
 
 
   getPopularMovies(){
-    return this._http$.get<Movies>(environment.apiUrl + '/movie/popular')
+    return this._http$.get<Movies>(environment.apiUrl + '/movie/popular').pipe(
+      map(res => res.results.map(movies => ({...movies, poster_path: environment.mediaUrl + movies.poster_path,
+        backdrop_path: movies.backdrop_path})))
+    )
   }
   getMovieById(id:number){
-    return this._http$.get<MovieId>(environment.apiUrl + `/movie/${id}`)
+    return this._http$.get<MovieId>(environment.apiUrl + `/movie/${id}`).pipe(
+      map(res => ({...res, poster_path: environment.mediaUrl + res.poster_path,
+        backdrop_path: res.backdrop_path}))
+    )
   }
 }
--- src/app/api/infra/watch-list.infra.ts
+++ src/app/api/infra/watch-list.infra.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {Movies} from '../entities/movies.entity';
 import {WatchList} from '../entities/watchList.entity';
+import {map} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -28,7 +29,10 @@ export class WatchListInfra{
   }
 
   getMoviesWatchlist(accountId: number){
-    return this._http$.get<WatchList>(environment.apiUrl + `/account/${accountId}/watchlist/movies`)
+    return this._http$.get<WatchList>(environment.apiUrl + `/account/${accountId}/watchlist/movies`).pipe(
+      map(res => res.results.map(movies => ({...movies, poster_path: environment.mediaUrl + movies.poster_path,
+        backdrop_path: movies.backdrop_path})))
+    )
   }
 
 
